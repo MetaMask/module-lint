2bf782a74f0f48c3ce0d2f3c5871ac1c3535677c
Elliot Winkler <elliot.winkler@gmail.com>
2023-11-06 10:24:24 -0700

    Remove unnecessary logging code

--- tests/helpers.ts
+++ tests/helpers.ts
@@ -1,9 +1,5 @@
 import { createSandbox } from '@metamask/utils/node';
 
-import { createModuleLogger, projectLogger } from '../src/logging-utils';
-
 const { withinSandbox } = createSandbox('module-lint-tests');
-
-export const log = createModuleLogger(projectLogger, 'tests');
 
 export { withinSandbox };
