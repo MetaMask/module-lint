bc3f58e12c001be24380104f9e83fd78f38d1780
Elliot Winkler <elliot.winkler@gmail.com>
2023-12-20 16:53:15 -0700

    Refactor require-source-directory and simplify message

--- src/rules/require-source-directory.ts
+++ src/rules/require-source-directory.ts
@@ -7,13 +7,18 @@ export default buildRule({
   dependencies: [],
   execute: async ({ project, pass, fail }) => {
     const entryPath = 'src';
-    const stats = await project.fs.getEntryStats(entryPath);
-    const passed = stats?.isDirectory();
-    const message = stats
-      ? `\`${entryPath}\` is not a directory when it should be.`
-      : `\`${entryPath}\` exists in the module template, but not in this repo.`;
-    const failures = [{ message }];
 
-    return passed ? pass() : fail(failures);
+    const stats = await project.fs.getEntryStats(entryPath);
+
+    if (stats?.isDirectory()) {
+      return pass();
+    }
+
+    const message =
+      stats && !stats.isDirectory()
+        ? `\`${entryPath}\` is not a directory when it should be.`
+        : `\`${entryPath}\` does not exist in this repo.`;
+    const failures = [{ message }];
+    return fail(failures);
   },
 });
