8b53d273733f9ff0a69d21145de141addd2be984
Elliot Winkler <elliot.winkler@gmail.com>
2023-12-21 09:35:01 -0700

    Refactor the check in  and document it

--- src/output-logger.ts
+++ src/output-logger.ts
@@ -82,12 +82,18 @@ export class OutputLogger implements AbstractOutputLogger {
 /**
  * Writes a line to the given stream.
  *
+ * If the first argument is a format string, it is passed to `util.format` along
+ * with the remaining arguments; otherwise, each argument is passed to
+ * `util.inspect` and then the results are concatenated. This is the same
+ * behavior as `console.log`, `console.error`, etc.
+ *
  * @param stream - The stream.
  * @param args - Arguments to `stream.write`: either one or more messages, or a
  * format string followed by values.
+ * @see {@link console.log}
  */
 export function logToStream(stream: SimpleWriteStream, args: unknown[]) {
-  if (typeof args[0] === 'string' && /%[sdifjoOc%]/u.test(args[0])) {
+  if (isFormatString(args[0])) {
     stream.write(`${format(args[0], ...args.slice(1))}\n`);
   } else {
     stream.write(
@@ -96,4 +102,17 @@ export function logToStream(stream: SimpleWriteStream, args: unknown[]) {
         .join(' ')}\n`,
     );
   }
+}
+
+/**
+ * Determines whether the given argument is a string that `util.format` takes by
+ * looking for the presence of a format specifier (one of `%s`, `%d`, `%i`,
+ * `%f`, `%j`, `%o`, `%O`, `%c`, or `%%`).
+ *
+ * @param value - The value to check.
+ * @see {@link format}
+ * @returns True if the given value is a format string, or false otherwise.
+ */
+function isFormatString(value: unknown) {
+  return typeof value === 'string' && /%[sdifjoOc%]/u.test(value);
 }
