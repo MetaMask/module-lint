c8c06c6e469eb98a15f2d4fe751e0923563b4985
Elliot Winkler <elliot.winkler@gmail.com>
2023-12-21 09:23:27 -0700

    Refactor better
    Co-authored-by: Mark Stacey <markjstacey@gmail.com>

--- src/rules/require-source-directory.ts
+++ src/rules/require-source-directory.ts
@@ -10,15 +10,12 @@ export default buildRule({
 
     const stats = await project.fs.getEntryStats(entryPath);
 
-    if (stats?.isDirectory()) {
-      return pass();
+    if (!stats) {
+      return fail([{ message: `\`${entryPath}/\` does not exist in this project.` }]);
+    } else if (!stats.isDirectory()) {
+      return fail([{ message: `\`${entryPath}/\` is not a directory when it should be.` }]);
     }
 
-    const message =
-      stats && !stats.isDirectory()
-        ? `\`${entryPath}/\` is not a directory when it should be.`
-        : `\`${entryPath}/\` does not exist in this project.`;
-    const failures = [{ message }];
-    return fail(failures);
+    return pass();
   },
 });
