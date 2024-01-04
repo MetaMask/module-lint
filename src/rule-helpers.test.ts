b683cd0656ce5966a3e022ecfd9b282ae28cda58
Elliot Winkler <elliot.winkler@gmail.com>
2024-01-04 15:54:25 -0700

    Add tests for combineRuleExecutionResults, fileConforms, and directoryAndContentsConform

--- src/rule-helpers.test.ts
+++ src/rule-helpers.test.ts
@@ -4,7 +4,15 @@ import {
 } from '@metamask/utils/node';
 import path from 'path';
 
-import { directoryExists, fail, fileExists, pass } from './rule-helpers';
+import {
+  combineRuleExecutionResults,
+  directoryAndContentsConform,
+  directoryExists,
+  fail,
+  fileConforms,
+  fileExists,
+  pass,
+} from './rule-helpers';
 import { buildMetaMaskRepository, withinSandbox } from '../tests/helpers';
 
 describe('pass', () => {
@@ -18,6 +26,49 @@ describe('fail', () => {
     expect(fail([{ message: 'oops' }])).toStrictEqual({
       passed: false,
       failures: [{ message: 'oops' }],
+    });
+  });
+});
+
+describe('combineRuleExecutionResults', () => {
+  it('returns a single passing result if all of the given results are passing', () => {
+    const result = combineRuleExecutionResults([
+      {
+        passed: true,
+      },
+      {
+        passed: true,
+      },
+    ]);
+
+    expect(result).toStrictEqual({ passed: true });
+  });
+
+  it('returns a single failed result, consolidating all failures, if any of the given results are failing', () => {
+    const result = combineRuleExecutionResults([
+      {
+        passed: true,
+      },
+      {
+        passed: false,
+        failures: [{ message: 'message 1' }],
+      },
+      {
+        passed: true,
+      },
+      {
+        passed: false,
+        failures: [{ message: 'message 2' }, { message: 'message 3' }],
+      },
+    ]);
+
+    expect(result).toStrictEqual({
+      passed: false,
+      failures: [
+        { message: 'message 1' },
+        { message: 'message 2' },
+        { message: 'message 3' },
+      ],
     });
   });
 });
@@ -167,6 +218,345 @@ describe('directoryExists', () => {
         failures: [
           {
             message: `\`${directoryPath}/\` is not a directory when it should be.`,
+          },
+        ],
+      });
+    });
+  });
+});
+
+describe('fileConforms', () => {
+  it('passes if the project and template have the same referenced file with the same content', async () => {
+    await withinSandbox(async (sandbox) => {
+      const template = buildMetaMaskRepository({
+        shortname: 'template',
+        directoryPath: path.join(sandbox.directoryPath, 'template'),
+      });
+      await writeFile(
+        path.join(template.directoryPath, 'some.file'),
+        'some content',
+      );
+      const project = buildMetaMaskRepository({
+        shortname: 'project',
+        directoryPath: path.join(sandbox.directoryPath, 'project'),
+      });
+      await writeFile(
+        path.join(project.directoryPath, 'some.file'),
+        'some content',
+      );
+
+      const result = await fileConforms('some.file', {
+        template,
+        project,
+        pass,
+        fail,
+      });
+
+      expect(result).toStrictEqual({ passed: true });
+    });
+  });
+
+  it('fails if the project has the same referenced file as the template, but its content does not match', async () => {
+    await withinSandbox(async (sandbox) => {
+      const template = buildMetaMaskRepository({
+        shortname: 'template',
+        directoryPath: path.join(sandbox.directoryPath, 'template'),
+      });
+      await writeFile(
+        path.join(template.directoryPath, 'some.file'),
+        'some content',
+      );
+      const project = buildMetaMaskRepository({
+        shortname: 'project',
+        directoryPath: path.join(sandbox.directoryPath, 'project'),
+      });
+      await writeFile(
+        path.join(project.directoryPath, 'some.file'),
+        'different content',
+      );
+
+      const result = await fileConforms('some.file', {
+        template,
+        project,
+        pass,
+        fail,
+      });
+
+      expect(result).toStrictEqual({
+        passed: false,
+        failures: [
+          {
+            message:
+              '`some.file` does not match the same file in the template repo.',
+          },
+        ],
+      });
+    });
+  });
+
+  it('fails if the project does not have the same referenced file as the template', async () => {
+    await withinSandbox(async (sandbox) => {
+      const template = buildMetaMaskRepository({
+        shortname: 'template',
+        directoryPath: path.join(sandbox.directoryPath, 'template'),
+      });
+      await writeFile(
+        path.join(template.directoryPath, 'some.file'),
+        'different content',
+      );
+      const project = buildMetaMaskRepository({
+        shortname: 'project',
+        directoryPath: path.join(sandbox.directoryPath, 'project'),
+      });
+
+      const result = await fileConforms('some.file', {
+        template,
+        project,
+        pass,
+        fail,
+      });
+
+      expect(result).toStrictEqual({
+        passed: false,
+        failures: [
+          {
+            message: '`some.file` does not exist in this project.',
+          },
+        ],
+      });
+    });
+  });
+});
+
+describe('directoryAndContentsConform', () => {
+  it('passes if the project and template have the same referenced directory with the same file structure, and all files have the same content', async () => {
+    await withinSandbox(async (sandbox) => {
+      const template = buildMetaMaskRepository({
+        shortname: 'template',
+        directoryPath: path.join(sandbox.directoryPath, 'template'),
+      });
+      await writeFile(
+        path.join(
+          template.directoryPath,
+          'some-directory',
+          'child-directory',
+          'some.file',
+        ),
+        'some content',
+      );
+      await writeFile(
+        path.join(
+          template.directoryPath,
+          'some-directory',
+          'child-directory',
+          'grandchild-directory',
+          'some.file',
+        ),
+        'some content',
+      );
+      const project = buildMetaMaskRepository({
+        shortname: 'project',
+        directoryPath: path.join(sandbox.directoryPath, 'project'),
+      });
+      await writeFile(
+        path.join(
+          project.directoryPath,
+          'some-directory',
+          'child-directory',
+          'some.file',
+        ),
+        'some content',
+      );
+      await writeFile(
+        path.join(
+          project.directoryPath,
+          'some-directory',
+          'child-directory',
+          'grandchild-directory',
+          'some.file',
+        ),
+        'some content',
+      );
+
+      const result = await directoryAndContentsConform('some-directory', {
+        template,
+        project,
+        pass,
+        fail,
+      });
+
+      expect(result).toStrictEqual({ passed: true });
+    });
+  });
+
+  it('fails if the project and template have the same referenced directory with the same file structure, but some files do not have the same content', async () => {
+    await withinSandbox(async (sandbox) => {
+      const template = buildMetaMaskRepository({
+        shortname: 'template',
+        directoryPath: path.join(sandbox.directoryPath, 'template'),
+      });
+      await writeFile(
+        path.join(
+          template.directoryPath,
+          'some-directory',
+          'child-directory',
+          'some.file',
+        ),
+        'some content',
+      );
+      await writeFile(
+        path.join(
+          template.directoryPath,
+          'some-directory',
+          'child-directory',
+          'grandchild-directory',
+          'some.file',
+        ),
+        'some content',
+      );
+      const project = buildMetaMaskRepository({
+        shortname: 'project',
+        directoryPath: path.join(sandbox.directoryPath, 'project'),
+      });
+      await writeFile(
+        path.join(
+          project.directoryPath,
+          'some-directory',
+          'child-directory',
+          'some.file',
+        ),
+        'some content',
+      );
+      await writeFile(
+        path.join(
+          project.directoryPath,
+          'some-directory',
+          'child-directory',
+          'grandchild-directory',
+          'some.file',
+        ),
+        'different content',
+      );
+
+      const result = await directoryAndContentsConform('some-directory', {
+        template,
+        project,
+        pass,
+        fail,
+      });
+
+      expect(result).toStrictEqual({
+        passed: false,
+        failures: [
+          {
+            message:
+              '`some-directory/child-directory/grandchild-directory/some.file` does not match the same file in the template repo.',
+          },
+        ],
+      });
+    });
+  });
+
+  it('fails if the project and template have the same referenced directory, but it does not have the same file structure', async () => {
+    await withinSandbox(async (sandbox) => {
+      const template = buildMetaMaskRepository({
+        shortname: 'template',
+        directoryPath: path.join(sandbox.directoryPath, 'template'),
+      });
+      await writeFile(
+        path.join(
+          template.directoryPath,
+          'some-directory',
+          'child-directory',
+          'some.file',
+        ),
+        'some content',
+      );
+      await writeFile(
+        path.join(
+          template.directoryPath,
+          'some-directory',
+          'child-directory',
+          'grandchild-directory',
+          'some.file',
+        ),
+        'some content',
+      );
+      const project = buildMetaMaskRepository({
+        shortname: 'project',
+        directoryPath: path.join(sandbox.directoryPath, 'project'),
+      });
+      await writeFile(
+        path.join(
+          project.directoryPath,
+          'some-directory',
+          'child-directory',
+          'some.file',
+        ),
+        'some content',
+      );
+
+      const result = await directoryAndContentsConform('some-directory', {
+        template,
+        project,
+        pass,
+        fail,
+      });
+
+      expect(result).toStrictEqual({
+        passed: false,
+        failures: [
+          {
+            message:
+              '`some-directory/child-directory/grandchild-directory/some.file` does not exist in this project.',
+          },
+        ],
+      });
+    });
+  });
+
+  it('fails if the project does not have the same referenced directory as the template', async () => {
+    await withinSandbox(async (sandbox) => {
+      const template = buildMetaMaskRepository({
+        shortname: 'template',
+        directoryPath: path.join(sandbox.directoryPath, 'template'),
+      });
+      await writeFile(
+        path.join(
+          template.directoryPath,
+          'some-directory',
+          'child-directory',
+          'some.file',
+        ),
+        'some content',
+      );
+      await writeFile(
+        path.join(
+          template.directoryPath,
+          'some-directory',
+          'child-directory',
+          'grandchild-directory',
+          'some.file',
+        ),
+        'some content',
+      );
+      const project = buildMetaMaskRepository({
+        shortname: 'project',
+        directoryPath: path.join(sandbox.directoryPath, 'project'),
+      });
+
+      const result = await directoryAndContentsConform('some-directory', {
+        template,
+        project,
+        pass,
+        fail,
+      });
+
+      expect(result).toStrictEqual({
+        passed: false,
+        failures: [
+          {
+            message: '`some-directory/` does not exist in this project.',
           },
         ],
       });
