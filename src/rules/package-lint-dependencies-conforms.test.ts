0fa421b26f3d126e77cadcfa2ebb31f09e980fc3
Kanthesha Devaramane <kanthesha.devaramane@consensys.net>
2024-01-18 11:52:26 +0000

    added lint related dependencies conform and related unit tests

--- src/rules/package-lint-dependencies-conforms.test.ts
+++ src/rules/package-lint-dependencies-conforms.test.ts
@@ -1,12 +1,12 @@
 import { writeFile } from '@metamask/utils/node';
 import path from 'path';
 
-import packageEnginesNodeFieldConforms from './package-engines-node-field-conforms';
+import packageLintDependenciesConforms from './package-lint-dependencies-conforms';
 import { buildMetaMaskRepository, withinSandbox } from '../../tests/helpers';
 import { fail, pass } from '../rule-helpers';
 
-describe('Rule: package-engines-node-field-conforms', () => {
-  it('passes if the "engines.node" field in the project\'s package.json matches the one in the template\'s package.json', async () => {
+describe('Rule: package-lint-dependencies-conforms', () => {
+  it("passes if the lint related dependencies in the project's package.json matches the one in the template's package.json", async () => {
     await withinSandbox(async (sandbox) => {
       const template = buildMetaMaskRepository({
         shortname: 'template',
@@ -33,7 +33,7 @@ describe('Rule: package-engines-node-field-conforms', () => {
         }),
       );
 
-      const result = await packageEnginesNodeFieldConforms.execute({
+      const result = await packageLintDependenciesConforms.execute({
         template,
         project,
         pass,
@@ -46,7 +46,7 @@ describe('Rule: package-engines-node-field-conforms', () => {
     });
   });
 
-  it('fails if the "engines.node" field in the project\'s package.json does not match the one in the template\'s package.json', async () => {
+  it("fails if the version of lint related dependencies in the project's package.json does not match the one in the template's package.json", async () => {
     await withinSandbox(async (sandbox) => {
       const template = buildMetaMaskRepository({
         shortname: 'template',
@@ -56,8 +56,8 @@ describe('Rule: package-engines-node-field-conforms', () => {
         path.join(template.directoryPath, 'package.json'),
         JSON.stringify({
           packageManager: 'a',
-          engines: { node: 'test1' },
-          devDependencies: { eslint: '1.0.0' },
+          engines: { node: 'test' },
+          devDependencies: { eslint: '1.1.0' },
         }),
       );
       const project = buildMetaMaskRepository({
@@ -68,12 +68,12 @@ describe('Rule: package-engines-node-field-conforms', () => {
         path.join(project.directoryPath, 'package.json'),
         JSON.stringify({
           packageManager: 'a',
-          engines: { node: 'test2' },
+          engines: { node: 'test' },
           devDependencies: { eslint: '1.0.0' },
         }),
       );
 
-      const result = await packageEnginesNodeFieldConforms.execute({
+      const result = await packageLintDependenciesConforms.execute({
         template,
         project,
         pass,
@@ -83,9 +83,50 @@ describe('Rule: package-engines-node-field-conforms', () => {
       expect(result).toStrictEqual({
         passed: false,
         failures: [
-          {
-            message: '`engines.node` is "test2", when it should be "test1".',
-          },
+          { message: 'eslint version is "1.0.0", when it should be "1.1.0".' },
+        ],
+      });
+    });
+  });
+
+  it("fails if the lint related dependency exist in the template's package.json, but not in the project's package.json", async () => {
+    await withinSandbox(async (sandbox) => {
+      const template = buildMetaMaskRepository({
+        shortname: 'template',
+        directoryPath: path.join(sandbox.directoryPath, 'template'),
+      });
+      await writeFile(
+        path.join(template.directoryPath, 'package.json'),
+        JSON.stringify({
+          packageManager: 'a',
+          engines: { node: 'test' },
+          devDependencies: { eslint: '1.1.0' },
+        }),
+      );
+      const project = buildMetaMaskRepository({
+        shortname: 'project',
+        directoryPath: path.join(sandbox.directoryPath, 'project'),
+      });
+      await writeFile(
+        path.join(project.directoryPath, 'package.json'),
+        JSON.stringify({
+          packageManager: 'a',
+          engines: { node: 'test' },
+          devDependencies: { testlint: '1.0.0' },
+        }),
+      );
+
+      const result = await packageLintDependenciesConforms.execute({
+        template,
+        project,
+        pass,
+        fail,
+      });
+
+      expect(result).toStrictEqual({
+        passed: false,
+        failures: [
+          { message: '`package.json` should contain "eslint", but does not.' },
         ],
       });
     });
